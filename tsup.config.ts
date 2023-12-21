fe9ee16b06750e9150a5e431eda2412344a73370
Burner <10004812+BurnerWah@users.noreply.github.com>
2023-12-21 15:04:40 -0600

    Tweaked tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,8 +3,9 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   entry: ['server/server.ts'],
   outDir: 'out',
-  splitting: false,
+  splitting: true,
   format: 'esm',
   sourcemap: true,
   clean: true,
+  target: 'node20',
 })
