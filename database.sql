30ce3ac86a6ac4799578b0a7e981c0ca11b5600d
Burner <10004812+BurnerWah@users.noreply.github.com>
2023-12-23 10:20:36 -0600

    Added base mode database design

--- database.sql
+++ database.sql
@@ -1,10 +1,39 @@
+CREATE TABLE IF NOT EXISTS
+    users (
+        id serial PRIMARY KEY,
+        username varchar(64) UNIQUE NOT NULL,
+        password_hash bytea NOT NULL,
+        password_salt bytea NOT NULL,
+        created_at timestamp NOT NULL DEFAULT NOW(),
+        administrator boolean NOT NULL DEFAULT FALSE
+    );
 
--- USER is a reserved keyword with Postgres
--- You must use double quotes in every query that user is in:
--- ex. SELECT * FROM "user";
--- Otherwise you will have errors!
-CREATE TABLE "user" (
-    "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
-);
+CREATE TABLE IF NOT EXISTS
+    object_types (
+        id serial PRIMARY KEY,
+        name varchar(64) UNIQUE NOT NULL
+    );
+
+CREATE TABLE IF NOT EXISTS
+    objects (
+        id serial PRIMARY KEY,
+        name varchar(256) NOT NULL,
+        created_by integer NOT NULL REFERENCES users (id),
+        created_at timestamp NOT NULL DEFAULT NOW(),
+        type_id integer NOT NULL REFERENCES object_types (id),
+        right_ascension time,
+        declination float,
+        distance numrange,
+        apparent_magnitude float,
+        absolute_magnitude float,
+        mass float,
+        redshift float,
+        nasa_image_id varchar(64)
+    );
+
+CREATE TABLE IF NOT EXISTS
+    users_objects (
+        user_id integer REFERENCES users (id),
+        object_id integer REFERENCES objects (id),
+        PRIMARY KEY (user_id, object_id)
+    );
