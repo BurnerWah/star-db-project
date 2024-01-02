3bd5f11613cf447fb457a12ab82b729dc023068a
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-01-02 16:09:07 -0600

    Added typedefs for object type and objects

--- types/tables.ts
+++ types/tables.ts
@@ -5,3 +5,33 @@ export interface DBUser {
   created_at: Date
   admin: boolean
 }
+
+export enum EDBObjectTypes {
+  Star = 1,
+  Planet = 2,
+  Galaxy = 3,
+  Nebula = 4,
+  Cluster = 5,
+  'Black Hole' = 6,
+}
+
+export interface DBObjectType {
+  id: EDBObjectTypes | number
+  name: keyof typeof EDBObjectTypes
+}
+
+export interface DBObject {
+  id: number
+  name: string
+  created_by: DBUser['id']
+  created_at: Date
+  type: DBObjectType['id']
+  right_ascension?: Date
+  declination?: number
+  distance?: unknown // I need to see what this looks like form pg
+  apparent_magnitude?: number
+  absolute_magnitude?: number
+  mass?: number
+  redshift?: number
+  nasa_image_id?: string
+}
