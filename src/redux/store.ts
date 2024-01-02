7fe1f70db82e5259625e278915a3a4fabfda719a
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-01-02 14:27:02 -0600

    Simplified dev tools composition to use the provided `composeWithDevToolsDevelopmentOnly` function

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,4 +1,4 @@
-import { composeWithDevTools } from '@redux-devtools/extension'
+import { composeWithDevToolsDevelopmentOnly } from '@redux-devtools/extension'
 import { applyMiddleware, legacy_createStore as createStore } from 'redux'
 import logger from 'redux-logger'
 import createSagaMiddleware from 'redux-saga'
@@ -7,19 +7,13 @@ import rootSaga from './sagas'
 
 const saga = createSagaMiddleware()
 
-// Check if we're in development, and if not, don't include the redux dev tools
-// A lot of sites include it in production (which is *extremely* funny), but
-// it's definitely not necessary.
-// Info on the dev tools: https://github.com/reduxjs/redux-devtools
-const nodeenv = process.env['NODE_ENV'] || 'development'
-const enhancers =
-  nodeenv === 'development'
-    ? composeWithDevTools(applyMiddleware(saga, logger))
-    : applyMiddleware(saga)
-
 // Creates a redux store with our reducers, middleware, and if in development,
 // the redux dev tools.
-const store = createStore(rootReducer, enhancers)
+const store = createStore(
+  rootReducer,
+  // Excludes dev tools in production
+  composeWithDevToolsDevelopmentOnly(applyMiddleware(saga, logger)),
+)
 
 // tells the saga middleware to use the rootSaga
 // rootSaga contains all of our other sagas
