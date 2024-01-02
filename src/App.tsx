dbeaaeccbe0d4c3d1eab165aac18333cf01fbc88
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-01-02 10:22:29 -0600

    Switched from using ProtectedRoute to render functions for routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Redirect, Route, HashRouter as Router, Switch } from 'react-router-dom'
 import './App.css'
 import Footer from './components/Footer/Footer.tsx'
 import Nav from './components/Nav/Nav.tsx'
-import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute.tsx'
+import RequireAuth from './components/RequireAuth.tsx'
 import { useAppDispatch, useAppSelector } from './hooks/redux.ts'
 import AboutPage from './pages/About'
 import InfoPage from './pages/Info'
@@ -43,21 +43,39 @@ function App() {
             Visiting localhost:3000/user will show the UserPage if the user is logged in.
             If the user is not logged in, the ProtectedRoute will show the LoginPage (component).
             Even though it seems like they are different pages, the user is always on localhost:3000/user */}
-          <ProtectedRoute
+          <Route
+            exact
+            path="/user"
+            render={() => (
+              <RequireAuth>
+                <UserPage />
+              </RequireAuth>
+            )}
+          />
+          {/* <ProtectedRoute
             // logged in shows UserPage else shows LoginPage
             exact
             path="/user"
           >
             <UserPage />
-          </ProtectedRoute>
+          </ProtectedRoute> */}
 
-          <ProtectedRoute
+          <Route
+            exact
+            path="/info"
+            render={() => (
+              <RequireAuth>
+                <InfoPage />
+              </RequireAuth>
+            )}
+          />
+          {/* <ProtectedRoute
             // logged in shows InfoPage else shows LoginPage
             exact
             path="/info"
           >
             <InfoPage />
-          </ProtectedRoute>
+          </ProtectedRoute> */}
 
           <Route exact path="/login">
             {user.id ? (
