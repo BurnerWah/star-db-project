dfb245531c4181399609e2ed9f04365a2aa6ce75
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-01-02 14:57:45 -0600

    Changed store to work with redux v5
    The main things this required was removing the redux logger (which appears to have been abandoned anyways), and adding an undefined preloadedstate to not confuse typescript

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,6 +1,5 @@
 import { composeWithDevToolsDevelopmentOnly } from '@redux-devtools/extension'
 import { applyMiddleware, legacy_createStore as createStore } from 'redux'
-import logger from 'redux-logger'
 import createSagaMiddleware from 'redux-saga'
 import rootReducer from './reducers'
 import rootSaga from './sagas'
@@ -11,8 +10,14 @@ const saga = createSagaMiddleware()
 // the redux dev tools.
 const store = createStore(
   rootReducer,
+  // Typescript gets confused by createStore's various definitions, because the
+  // 2nd argument can be an enhancer *or* the preloaded state. This is the
+  // easiest way to tell it that it's fine.
+  undefined,
   // Excludes dev tools in production
-  composeWithDevToolsDevelopmentOnly(applyMiddleware(saga, logger)),
+  // We can't use redux-logger with redux v5, but the dev tools are here so
+  // that should be fine.
+  composeWithDevToolsDevelopmentOnly(applyMiddleware(saga)),
 )
 
 // tells the saga middleware to use the rootSaga
