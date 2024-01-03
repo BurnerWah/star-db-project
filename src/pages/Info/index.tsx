adcfbf24a1c7fe2fe2170e91eb73a9d043b96569
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-01-03 12:31:00 -0600

    Removed unnecessary comment

--- src/pages/Info/index.tsx
+++ src/pages/Info/index.tsx
@@ -1,8 +1,3 @@
-// This is one of our simplest components
-// It doesn't have local state
-// It doesn't dispatch any redux actions or display any part of redux state
-// or even care what the redux state is
-
 function InfoPage() {
   return (
     <div className="container">
