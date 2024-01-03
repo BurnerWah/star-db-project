a4b05b69ba2056f71a9f0bbac7a6e2313325444b
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-01-02 21:30:44 -0600

    Added full parssing of the data from the DB

--- server/routes/items.ts
+++ server/routes/items.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import { MeasurementWithUncertainty } from '~typings/structs.ts'
+import { ParsedItem } from '~typings/structs.ts'
 import { DBObject, DBObjectType } from '~typings/tables.ts'
 import { parseDeclination } from '../db/normalizers.ts'
 import pool from '../db/pool.ts'
@@ -20,7 +20,6 @@ type ItemQuery = Pick<
   | 'absolute_magnitude'
   | 'mass'
   | 'redshift'
-  | 'nasa_image_id'
 > & {
   [Column in keyof DBObjectType as `type_${Column}`]: DBObjectType[Column]
 }
@@ -41,34 +40,32 @@ items.get('/', async (req, res) => {
 	      o.absolute_magnitude,
 	      o.mass,
 	      o.redshift,
-	      o.nasa_image_id
       FROM
 	      objects AS o
 	      INNER JOIN object_types AS t ON o.type_id = t.id;
     `)
-    const items = result.rows.map((item) => ({
+    const items: ParsedItem[] = result.rows.map((item) => ({
       id: item.id,
       name: item.name,
       type: {
         id: item.type_id,
         name: item.type_name,
-      } as DBObjectType,
+      },
       right_ascension: item.right_ascension,
       declination: item.declination
         ? parseDeclination(item.declination)
         : undefined,
       distance:
         item.distance && item.distance_error
-          ? ({
+          ? {
               value: item.distance,
               error: item.distance_error,
-            } as MeasurementWithUncertainty)
+            }
           : undefined,
       apparent_magnitude: item.apparent_magnitude,
       absolute_magnitude: item.absolute_magnitude,
       mass: item.mass,
       redshift: item.redshift,
-      nasa_image_id: item.nasa_image_id,
     }))
     res.send(items)
   } catch (error) {
--- types/structs.ts
+++ types/structs.ts
@@ -1,3 +1,5 @@
+import { DBObject, DBObjectType } from './tables'
+
 export interface Declination {
   sign: -1 | 0 | 1
   degrees: number
@@ -8,4 +10,17 @@ export interface Declination {
 export interface MeasurementWithUncertainty {
   value: number
   error: number
+}
+
+export interface ParsedItem {
+  id: DBObject['id']
+  name: DBObject['name']
+  type: DBObjectType
+  right_ascension?: DBObject['right_ascension']
+  declination?: Declination | undefined
+  distance?: MeasurementWithUncertainty | undefined
+  apparent_magnitude?: DBObject['apparent_magnitude']
+  absolute_magnitude?: DBObject['absolute_magnitude']
+  mass?: DBObject['mass']
+  redshift?: DBObject['redshift']
 }
