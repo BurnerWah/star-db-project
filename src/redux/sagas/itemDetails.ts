89d99b0818d52f0b43a627749fb28b5fc0f3217e
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-01-03 09:31:01 -0600

    Added item details saga

---
+++ src/redux/sagas/itemDetails.ts
@@ -0,0 +1,22 @@
+import axios, { AxiosResponse } from 'axios'
+import { SagaIterator } from 'redux-saga'
+import { call, takeLatest } from 'redux-saga/effects'
+import { FetchItemDetailsSaga } from '~typings/actions'
+import { ItemDetails } from '~typings/requests'
+import { put } from '../../hooks/redux'
+
+function* fetchItemDetails({ payload }: FetchItemDetailsSaga): SagaIterator {
+  try {
+    const response: AxiosResponse<ItemDetails> = yield call(
+      axios.get,
+      `/api/items/${payload}`,
+    )
+    yield put({ type: 'itemDetails/set', payload: response.data })
+  } catch (error) {
+    console.log('Item details get request failed', error)
+  }
+}
+
+export default function* itemDetailsSaga() {
+  yield takeLatest('itemDetails/fetch', fetchItemDetails)
+}
