1afd18f94218b38ae083a02f11507753a9cf1df8
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-01-03 13:23:36 -0600

    Added list saved items saga

--- src/redux/sagas/savedItems.ts
+++ src/redux/sagas/savedItems.ts
@@ -1,7 +1,9 @@
-import axios from 'axios'
+import axios, { AxiosResponse } from 'axios'
 import { SagaIterator } from 'redux-saga'
 import { call, takeLatest } from 'redux-saga/effects'
 import { SaveItemSaga } from '~typings/actions'
+import { ListItemsBody } from '~typings/requests'
+import { put } from '../../hooks/redux'
 
 function* saveItem({ payload }: SaveItemSaga): SagaIterator {
   try {
@@ -14,6 +16,29 @@ function* saveItem({ payload }: SaveItemSaga): SagaIterator {
   }
 }
 
+/**
+ * Lists saved items for the current user.
+ * This hijacks the listItems reducer to store the saved items since they're
+ * the same types, and won't ever be set at the same time.
+ */
+function* listSavedItems(): SagaIterator {
+  try {
+    yield put({ type: 'LIST_ITEMS::UNSET' })
+    const response: AxiosResponse<ListItemsBody> = yield call(
+      axios.get,
+      '/api/saved',
+      {
+        headers: { 'Content-Type': 'application/json' },
+        withCredentials: true,
+      },
+    )
+    yield put({ type: 'LIST_ITEMS::SET', payload: response.data })
+  } catch (error) {
+    console.log('Error listing saved items: ', error)
+  }
+}
+
 export default function* savedItemsSaga() {
   yield takeLatest('api/saveItem', saveItem)
+  yield takeLatest('api/listSavedItems', listSavedItems)
 }
