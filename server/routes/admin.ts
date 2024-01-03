8f7670f7dc71f0220726ab2a490a527dc23f7ba3
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-01-03 14:01:06 -0600

    Added delete item route for admin

--- server/routes/admin.ts
+++ server/routes/admin.ts
@@ -1,8 +1,26 @@
 import { Router } from 'express'
+import pool from '../db/pool.ts'
 import { rejectNonAdmin } from '../middleware/authentication.ts'
 
 const admin = Router()
 
 admin.use(rejectNonAdmin)
+
+admin.delete('/delete/:id', async (req, res) => {
+  const { id } = req.params
+  try {
+    const result = await pool.query(
+      /*sql*/ `
+        DELETE FROM objects WHERE id = $1;
+      `,
+      [id],
+    )
+    console.log(result)
+    res.sendStatus(200)
+  } catch (error) {
+    console.log(error)
+    res.sendStatus(500)
+  }
+})
 
 export default admin
