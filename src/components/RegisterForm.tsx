40510c183cb72ab608c5ba2c32abdb6a16d5471c
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-01-05 14:38:49 -0600

    Remade Register Form with shadcn, react-hook-form, and zod

--- src/components/RegisterForm.tsx
+++ src/components/RegisterForm.tsx
@@ -1,65 +1,88 @@
+import { TypographyH2 } from '@/components/typography'
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
 import { Button } from '@/components/ui/button'
-import { useState } from 'react'
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { ExclamationTriangleIcon } from '@radix-ui/react-icons'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
 import { useAppDispatch, useAppSelector } from '../hooks/redux'
+
+const formSchema = z.object({
+  username: z
+    .string()
+    .min(1, { message: 'Username is required.' })
+    .max(64, { message: 'Username must be less than 64 characters.' }),
+  password: z.string().min(8, {
+    message: 'Password must be at least 8 characters.',
+  }),
+})
 
 function RegisterForm() {
   const dispatch = useAppDispatch()
-
   const errors = useAppSelector((store) => store.errors)
 
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+  })
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    dispatch({ type: 'api/auth/register', payload: values })
+  }
 
   return (
-    <form
-      className="formPanel"
-      onSubmit={(e) => {
-        e.preventDefault()
-
-        dispatch({
-          type: 'api/auth/register',
-          payload: {
-            username: username,
-            password: password,
-          },
-        })
-      }}
-    >
-      <h2>Register User</h2>
+    <>
+      <TypographyH2>Register User</TypographyH2>
       {errors.registrationMessage && (
-        <h3 className="alert" role="alert">
-          {errors.registrationMessage}
-        </h3>
+        <Alert variant="destructive">
+          <ExclamationTriangleIcon className="h-4 w-4" />
+          <AlertTitle>Error</AlertTitle>
+          <AlertDescription>{errors.registrationMessage}</AlertDescription>
+        </Alert>
       )}
-      <div>
-        <label htmlFor="username">
-          Username:{' '}
-          <input
-            type="text"
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
             name="username"
-            value={username}
-            required
-            onChange={(e) => setUsername(e.target.value)}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input required {...field} />
+                </FormControl>
+                <FormDescription>Enter your username.</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
           />
-        </label>
-      </div>
-      <div>
-        <label htmlFor="password">
-          Password:{' '}
-          <input
-            type="password"
+          <FormField
+            control={form.control}
             name="password"
-            value={password}
-            required
-            onChange={(e) => setPassword(e.target.value)}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input type="password" required {...field} />
+                </FormControl>
+                <FormDescription>Enter your password.</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
           />
-        </label>
-      </div>
-      <div>
-        <Button type="submit">Register</Button>
-        {/* <input className="btn" type="submit" name="submit" value="Register" /> */}
-      </div>
-    </form>
+          <Button type="submit">Register</Button>
+        </form>
+      </Form>
+    </>
   )
 }
 
