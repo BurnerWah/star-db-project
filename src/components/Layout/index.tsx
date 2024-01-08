f91b3c73a1787d9810b446bf2557708ff41ea630
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-01-08 09:44:26 -0600

    Added toaster to layout

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,14 +1,18 @@
+import { Toaster } from '@/components/ui/toaster'
 import { Outlet } from 'react-router-dom'
 import Footer from './Footer'
 import Nav from './Nav'
-// import OldNav from './OldNav'
 
 export default function Layout() {
   return (
     <div>
-      {/* <OldNav /> */}
       <Nav />
-      <Outlet />
+      <body>
+        <main>
+          <Outlet />
+        </main>
+        <Toaster />
+      </body>
       <Footer />
     </div>
   )
