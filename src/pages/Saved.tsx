46f75da7af86a69015992e7569de5f2a4ced6ef4
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-01-08 10:25:07 -0600

    Moved the table from the saved item page into its own component

--- src/pages/Saved.tsx
+++ src/pages/Saved.tsx
@@ -1,86 +1,11 @@
+import ItemTable from '@/components/ItemTable'
 import { TypographyH2 } from '@/components/typography'
-import { Button } from '@/components/ui/button'
-import { DataTable } from '@/components/ui/data-table'
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu'
-import { createColumnHelper, type ColumnDef } from '@tanstack/react-table'
-import { MoreHorizontal } from 'lucide-react'
-import { useEffect } from 'react'
-import { Link } from 'react-router-dom'
-import { type ListItem } from '~typings/requests'
-import { useAppDispatch, useAppSelector } from '../hooks/redux'
-
-const columnHelper = createColumnHelper<ListItem>()
-
-const columns = [
-  columnHelper.accessor('name', { header: 'Name' }),
-  columnHelper.accessor('type.name', { header: 'Type' }),
-  columnHelper.accessor('right_ascension', { header: 'Right Ascension' }),
-  columnHelper.accessor(
-    ({ declination }) =>
-      declination &&
-      `${
-        declination?.sign * declination?.degrees
-      }° ${declination?.arcmin}' ${declination?.arcsec}"`,
-    { header: 'Declination' },
-  ),
-  columnHelper.accessor(
-    ({ distance }) => distance && `${distance.value}±${distance.error} Ly`,
-    { header: 'Distance' },
-  ),
-  columnHelper.group({
-    header: 'Magnitude',
-    columns: [
-      columnHelper.accessor('apparent_magnitude', { header: 'Apparent' }),
-      columnHelper.accessor('absolute_magnitude', { header: 'Absolute' }),
-    ],
-  }),
-  columnHelper.accessor('mass', { header: 'Mass' }),
-  columnHelper.accessor('redshift', { header: 'Redshift' }),
-  {
-    id: 'actions',
-    cell: ({ row }) => (
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button variant="ghost" className="h-8 w-8 p-0">
-            <span className="sr-only">Open Menu</span>
-            <MoreHorizontal className="h-4 w-4" />
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent align="end">
-          <DropdownMenuLabel>Actions</DropdownMenuLabel>
-          <DropdownMenuItem asChild>
-            <Link to={`/details/${row.original.id}`}>View Details</Link>
-          </DropdownMenuItem>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem
-            onClick={() => navigator.clipboard.writeText(row.original.name)}
-          >
-            Copy Name
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
-    ),
-  },
-] as ColumnDef<ListItem>[]
 
 export default function Saved() {
-  const dispatch = useAppDispatch()
-  const listItems = useAppSelector((state) => state.listItems)
-  useEffect(() => {
-    dispatch({ type: 'api/listSavedItems' })
-  }, [dispatch])
-
   return (
     <div className="container">
       <TypographyH2>Listing</TypographyH2>
-      <DataTable columns={columns} data={listItems} />
+      <ItemTable action={{ type: 'api/listSavedItems' }} />
     </div>
   )
 }
