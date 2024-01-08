b354ade141af1d354ded0663375137f7f2f14a4f
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-01-08 14:11:18 -0600

    Removed special css for footer

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,10 +1,10 @@
-import './Footer.css'
-
 // This is one of our simplest components
 // It doesn't have local state, so it can be a function component.
 // It doesn't dispatch any redux actions or display any part of redux state
 // or even care what the redux state is, so it doesn't need 'connect()'
 
 export default function Footer() {
-  return <footer>&copy; Jaden Pleasants</footer>
+  return (
+    <footer className="mt-20 p-5 text-center">&copy; Jaden Pleasants</footer>
+  )
 }
