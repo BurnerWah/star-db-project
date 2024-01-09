1f0984b96505eeadd3e8d7f8549249b20952c62a
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-01-09 09:49:54 -0600

    Added James Webb Space Telescope's first deep field image to the login window

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -11,6 +11,7 @@ import {
 } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
 import { useAppDispatch } from '@/hooks/redux'
+import { cn } from '@/lib/utils'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
 import { useNavigate } from 'react-router-dom'
@@ -32,9 +33,22 @@ export default function Login() {
   return (
     <div className="container relative hidden h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
       <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
-        <div className="absolute inset-0 bg-zinc-900" />
+        <div className={cn('absolute inset-0', 'bg-jwst-deep-field')} />
         <div className="relative z-20 flex items-center text-lg font-medium">
           StarForge
+        </div>
+        <div className="relative z-20 mt-auto">
+          <blockquote className="space-y-2">
+            <p className="text-lg">
+              {"Webb's First Deep Field (NIRCam Image)"}
+            </p>
+            <footer className="text-sm">
+              Credits:{' '}
+              <a href="https://webbtelescope.org/contents/media/images/2022/035/01G7DCWB7137MYJ05CSH1Q5Z1Z">
+                NASA, ESA, CSA, STScI
+              </a>
+            </footer>
+          </blockquote>
         </div>
       </div>
       <div className="lg:p-8">
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -73,6 +73,9 @@ export default {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
       },
+      backgroundImage: {
+        'jwst-deep-field': "url('assets/images/jwst-deep-field.png')",
+      },
     },
   },
   plugins: [tailwindCssAnimate],
