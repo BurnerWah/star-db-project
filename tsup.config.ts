4ea484b06678ece0b099e1c983062cbcea36abb3
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-01-08 19:18:29 -0600

    Fixed tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'tsup'
 export default defineConfig((options) => ({
   entry: ['server/server.ts'],
   outDir: 'dist/server',
+  tsconfig: 'tsconfig.server.json',
   splitting: true,
   format: 'esm',
   sourcemap: true,
