3accac699f42896707dd558f2941e1b970ef3c5d
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-01-09 16:42:08 -0600

    Redesigned footer and made header a component for future editing

--- src/layouts/core/Footer.tsx
+++ src/layouts/core/Footer.tsx
@@ -1,10 +1,11 @@
-// This is one of our simplest components
-// It doesn't have local state, so it can be a function component.
-// It doesn't dispatch any redux actions or display any part of redux state
-// or even care what the redux state is, so it doesn't need 'connect()'
-
 export default function Footer() {
   return (
-    <footer className="mt-20 p-5 text-center">&copy; Jaden Pleasants</footer>
+    <footer className="py-6">
+      <div className="container">
+        <p className="text-balance text-center text-sm text-muted-foreground">
+          &copy; Jaden Pleasants
+        </p>
+      </div>
+    </footer>
   )
 }
--- src/layouts/core/index.tsx
+++ src/layouts/core/index.tsx
@@ -2,13 +2,13 @@ import { Toaster } from '@/components/ui/toaster'
 import { Outlet } from 'react-router-dom'
 import Command from './Command'
 import Footer from './Footer'
-import Nav from './Nav'
+import Header from './Header'
 
 export function Layout() {
   return (
     <div>
       <body>
-        <Nav />
+        <Header />
         <Command />
         <main className="p-5">
           <Outlet />
