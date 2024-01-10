95318a0b02ea6298caab71e1c60ba468cc7fb994
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-01-09 19:12:37 -0600

    Improved command menu

--- src/layouts/core/Command.tsx
+++ src/layouts/core/Command.tsx
@@ -6,12 +6,15 @@ import {
   CommandItem,
   CommandList,
 } from '@/components/ui/command'
+import { useAppDispatch, useAppSelector } from '@/hooks/redux'
 import { useState } from 'react'
 import { useHotkeys } from 'react-hotkeys-hook'
 import { useNavigate, type NavigateOptions, type To } from 'react-router-dom'
 
 export default function Command() {
   const navigate = useNavigate()
+  const user = useAppSelector((store) => store.user)
+  const dispatch = useAppDispatch()
 
   const [open, setOpen] = useState(false)
 
@@ -34,6 +37,35 @@ export default function Command() {
           <CommandItem onSelect={() => goToPage('/list')}>List</CommandItem>
           <CommandItem onSelect={() => goToPage('/about')}>About</CommandItem>
         </CommandGroup>
+        {!user.id && (
+          <CommandGroup title="Authentication">
+            <CommandItem onSelect={() => goToPage('/login')}>Login</CommandItem>
+            <CommandItem onSelect={() => goToPage('/registration')}>
+              Register
+            </CommandItem>
+          </CommandGroup>
+        )}
+        {user.id && (
+          <CommandGroup title="User">
+            <CommandItem onSelect={() => goToPage('/user')}>
+              User Info
+            </CommandItem>
+            <CommandItem onSelect={() => goToPage('/saved')}>Saved</CommandItem>
+            <CommandItem onSelect={() => goToPage('/info')}>
+              Info Page
+            </CommandItem>
+            <CommandItem onSelect={() => dispatch({ type: 'api/auth/logout' })}>
+              Logout
+            </CommandItem>
+          </CommandGroup>
+        )}
+        {user.administrator && (
+          <CommandGroup title="Admin">
+            <CommandItem onSelect={() => goToPage('/add')}>
+              Add Item
+            </CommandItem>
+          </CommandGroup>
+        )}
       </CommandList>
     </CommandDialog>
   )
