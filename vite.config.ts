2dd556cc5cfb71b3ef3c70ac6b8fc25e9c058b29
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-01-10 13:16:53 -0600

    Added lightningcss, tweaked Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,12 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), imagetools()],
+  css: {
+    devSourcemap: true,
+  },
   build: {
     outDir: 'dist/client',
+    cssMinify: 'lightningcss',
   },
   server: {
     proxy: {
