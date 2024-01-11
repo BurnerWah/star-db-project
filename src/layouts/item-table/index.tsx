ef896c5aa93cceae3f297e2cd7b656bf0cfcc829
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-01-10 21:30:55 -0600

    Added Search Form

--- src/layouts/item-table/index.tsx
+++ src/layouts/item-table/index.tsx
@@ -1,10 +1,13 @@
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
 import { useAppDispatch, useAppSelector } from '@/hooks/redux'
 import {
   PaginationState,
   getCoreRowModel,
   useReactTable,
 } from '@tanstack/react-table'
-import { useEffect, type ReactNode } from 'react'
+import { Search } from 'lucide-react'
+import { FormEventHandler, useEffect, useState, type ReactNode } from 'react'
 import type { FetchListItemsSaga, ListSavedItemsSaga } from '~typings/actions'
 import { columns } from './columns'
 import { DirectDataTable } from './data-table'
@@ -15,8 +18,10 @@ export function ItemTable<A extends FetchListItemsSaga | ListSavedItemsSaga>({
 }: Readonly<{ action: A; title?: string }>) {
   const dispatch = useAppDispatch()
   const listItems = useAppSelector((state) => state.listItems)
-  const pageCount = useAppSelector((state) => state.listItems.pageCount ?? -1)
+  const pageCount = useAppSelector((state) => state.listItems.pageCount)
   const pagination = useAppSelector((state) => state.table.pagination)
+
+  const [search, setSearch] = useState('')
 
   const actionType = action.type
 
@@ -33,13 +38,22 @@ export function ItemTable<A extends FetchListItemsSaga | ListSavedItemsSaga>({
       )
       dispatch({
         type: actionType,
-        payload: { page: nextState.pageIndex },
+        payload: { page: nextState.pageIndex, search },
       } as A)
     },
     getCoreRowModel: getCoreRowModel(),
     manualPagination: true,
     debugTable: import.meta.env.DEV,
   })
+
+  const handleSearch: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault()
+    dispatch({
+      type: actionType,
+      payload: { search },
+    } as A)
+    setSearch('')
+  }
 
   useEffect(() => {
     dispatch(action)
@@ -48,6 +62,21 @@ export function ItemTable<A extends FetchListItemsSaga | ListSavedItemsSaga>({
   return (
     <div className="h-full flex-1 flex-col space-y-8 p-8 md:flex">
       {title && <TableTitle>{title}</TableTitle>}
+      <form
+        onSubmit={handleSearch}
+        className="flex items-center space-x-2 py-4"
+      >
+        <div className="relative">
+          <Search className="absolute left-2 top-2.5 h-4 w-4 text-muted-foreground" />
+          <Input
+            placeholder="Search ..."
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            className="max-w-sm pl-8"
+          />
+        </div>
+        <Button type="submit">Submit</Button>
+      </form>
       {listItems.items && (
         <DirectDataTable table={table} columns={columns} pagination />
       )}
