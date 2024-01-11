7fc82ea40c31eb931fec0908f9b0718b5d98e330
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-01-11 09:35:37 -0600

    Improved ESLint config for shadcn

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unknown-property */
 import { Dialog, DialogContent } from '@/components/ui/dialog'
 import { cn } from '@/lib/utils'
 import { type DialogProps } from '@radix-ui/react-dialog'
