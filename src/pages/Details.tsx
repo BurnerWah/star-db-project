9329855ad7eb75fb92c669f6c50c2acc6c52b197
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-01-11 12:34:35 -0600

    Redid details page

--- src/pages/Details.tsx
+++ src/pages/Details.tsx
@@ -1,66 +1,120 @@
-import { TypographyH2 } from '@/components/typography'
+import {
+  DeclinationTeX,
+  DistanceTeX,
+  MassTeX,
+  RightAscensionTeX,
+} from '@/components/formatters'
+import { TypographyH1 } from '@/components/typography'
 import { Button } from '@/components/ui/button'
-import { Card, CardHeader, CardTitle } from '@/components/ui/card'
-import { useEffect } from 'react'
+import { useEffect, type ReactNode } from 'react'
 import { useParams } from 'react-router-dom'
 import { useAppDispatch, useAppSelector } from '../hooks/redux'
 
+// Based loosely off of https://tailwindui.com/components/application-ui/data-display/description-lists
 export default function Details() {
-  const { id } = useParams()
+  const { id: pageId } = useParams()
+
   const dispatch = useAppDispatch()
-  const itemDetails = useAppSelector((state) => state.itemDetails)
-  const user = useAppSelector((state) => state.user)
+
+  const loggedIn = useAppSelector((state) => Boolean(state.user.id))
+  const administrator = useAppSelector((state) => state.user.administrator)
+
+  const id = useAppSelector((state) => state.itemDetails.id)
+  const name = useAppSelector((state) => state.itemDetails.name)
+  const type = useAppSelector((state) => state.itemDetails.type?.name)
+  const ra = useAppSelector((state) => state.itemDetails.right_ascension)
+  const declination = useAppSelector((state) => state.itemDetails.declination)
+  const distance = useAppSelector((state) => state.itemDetails.distance)
+  const apparentMagnitude = useAppSelector(
+    (state) => state.itemDetails.apparent_magnitude,
+  )
+  const absoluteMagnitude = useAppSelector(
+    (state) => state.itemDetails.absolute_magnitude,
+  )
+  const mass = useAppSelector((state) => state.itemDetails.mass)
+  const redshift = useAppSelector((state) => state.itemDetails.redshift)
+  const saved = useAppSelector((state) => state.itemDetails.saved)
 
   useEffect(() => {
-    if (id) {
-      dispatch({ type: 'itemDetails/fetch', payload: id })
+    if (pageId) {
+      dispatch({ type: 'itemDetails/fetch', payload: pageId })
     }
     return () => {
       dispatch({ type: 'itemDetails/unset' })
     }
-  }, [dispatch, id])
+  }, [dispatch, pageId])
 
   return (
     <div className="container mt-8">
-      <TypographyH2>{itemDetails.name}</TypographyH2>
-      <Card>
-        <CardHeader>
-          <CardTitle>{itemDetails.name}</CardTitle>
-        </CardHeader>
-      </Card>
-      {/* I don't wanna format this right now, I just want to validate that it works */}
-      <pre>{JSON.stringify(itemDetails, null, 2)}</pre>
-      {user.id &&
-        (itemDetails.saved ? (
+      <TypographyH1>
+        {name} ({type})
+      </TypographyH1>
+      <div className="mt-6 border-t">
+        <dl className="divide-y">
+          <DescriptionListItem label="Right Ascension">
+            {ra ? <RightAscensionTeX rightAscension={ra} /> : 'N/A'}
+          </DescriptionListItem>
+          <DescriptionListItem label="Declination">
+            {declination ? <DeclinationTeX declination={declination} /> : 'N/A'}
+          </DescriptionListItem>
+          <DescriptionListItem label="Distance">
+            {distance ? <DistanceTeX distance={distance} /> : 'N/A'}
+          </DescriptionListItem>
+          <DescriptionListItem label="Apparent Magnitude">
+            {apparentMagnitude || 'N/A'}
+          </DescriptionListItem>
+          <DescriptionListItem label="Absolute Magnitude">
+            {absoluteMagnitude || 'N/A'}
+          </DescriptionListItem>
+          <DescriptionListItem label="Mass">
+            {mass ? <MassTeX mass={mass} /> : 'N/A'}
+          </DescriptionListItem>
+          <DescriptionListItem label="Redshift">
+            {redshift || 'N/A'}
+          </DescriptionListItem>
+        </dl>
+      </div>
+      {loggedIn &&
+        (saved ? (
           <Button
-            onClick={() =>
-              dispatch({ type: 'api/unsaveItem', payload: itemDetails.id })
-            }
+            onClick={() => dispatch({ type: 'api/unsaveItem', payload: id })}
           >
             Unsave
           </Button>
         ) : (
           <Button
-            onClick={() =>
-              dispatch({ type: 'api/saveItem', payload: itemDetails.id })
-            }
+            onClick={() => dispatch({ type: 'api/saveItem', payload: id })}
           >
             Save
           </Button>
         ))}
-      {user.administrator && (
+      {administrator && (
         <Button
           variant="destructive"
           onClick={() =>
             dispatch({
               type: 'api/admin/deleteItem',
-              payload: { id: itemDetails.id },
+              payload: { id },
             })
           }
         >
           Delete
         </Button>
       )}
+    </div>
+  )
+}
+
+function DescriptionListItem({
+  label,
+  children,
+}: Readonly<{ label: ReactNode; children: ReactNode }>) {
+  return (
+    <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+      <dt className="text-sm font-medium leading-6">{label}</dt>
+      <dd className="mt-1 text-sm leading-6 sm:col-span-2 sm:mt-0">
+        {children}
+      </dd>
     </div>
   )
 }
