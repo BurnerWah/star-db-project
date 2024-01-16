93166d74cc0fddff732051199a3ececb3b408cef
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-01-16 13:02:40 -0600

    Fixed a typo

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -26,7 +26,7 @@ const About = memo(() => (
     <h1>About</h1>
     <p>
       {`
-      I've always found space fascinationg, but I feel like astronomy apps
+      I've always found space fascinating, but I feel like astronomy apps
       aren't really as focused on showing information about planets & stars as
       they are on rendering the sky. That's an important task, but sometimes I
       just want something a bit simpler. That's why I created this.
